f380cf2b6758960392872752a6829a5e5795acb5
himanshupatel2007 <himanshupatel.942007@gmail.com>
2026-01-17 13:46:49 +0530

    Transition for loading screen to main page is done

--- assests/files/JS/loading.js
+++ assests/files/JS/loading.js
@@ -3,20 +3,24 @@ const tl = gsap.timeline();
 
 tl.fromTo(".letter",
     {
+        
         opacity: 0,
         scale: 5,
         x: () => (Math.random() - 0.5) * 4000,
         y: () => (Math.random() - 0.5) * 4000,
         rotation: () => (Math.random() - 0.5) * 90
     },
-    {delay:2,
+    {
+        color: "#e31212",
+        webkitTextStroke: "3px #ac1911",
+        textShadow: "0 0 15px #ff0000, 0 0 30px #8b0000",
         opacity: 1,
         scale: 1,
         x: 0,
         y: 0,
         rotation: 0,
         duration: 10,
-        ease: "power4.out",
+        ease: "expoScale(0.5,7,none)",
         stagger: {
             amount: 1.5,
             from: "random"
@@ -29,18 +33,33 @@ tl.to(".bar", {
     width: '50%',
     duration: 2,
     ease: "power4.inOut"
-}, "-=2");
+}, "-=1");
 
 
-tl.to(".glow", {
+tl.to(".letter", {
     opacity: 0.8,
     duration: 0.1,
+    scale:0.98,
+    
     repeat: 5,
     yoyo: true,
     ease: "rough"
 });
 
-tl.to(".loadingScreen",{
+tl.to(".letter, .bar",{
     opacity:0,
-    duration:4
+    duration:4,
+    ease:"power2.inOut",
+    onComplete : ()=>{
+        gsap.set(".letter, .bar,.loadingScreen",{
+            display:"none",
+        })
+    }
 })
+
+tl.to(".finalBackground",{
+    opacity:1,
+    duration :8,
+    ease:"power4.out",
+
+},"<")
